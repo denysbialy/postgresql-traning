6bf5b1010edc77199a9c7a3f4813facdec78cf27
denysbialy <dnsbls11@gmail.com>
2022-06-13 19:28:27 +0300

    Add fixed bags

--- notYouTube.sql
+++ notYouTube.sql
@@ -1,24 +1,26 @@
 CREATE TABLE users_notyoutube (
-    id bigserial PRIMARY KEY,
+    user_id bigserial PRIMARY KEY,
     username VARCHAR(56) NOT NULL CHECK(username <> ''),
     email VARCHAR(124) NOT NULL UNIQUE CONSTRAINT "VALID EMAIL" CHECK (
         email ~ '^[A-Za-z0-9]\w{0,}@[a-z]{1,}\.[a-z]{1,}$'
     )
 );
 CREATE TABLE video (
-    id bigserial PRIMARY KEY,
+    video_id bigserial PRIMARY KEY,
     user_id int NOT NULL REFERENCES users_notyoutube,
-    video_name NOT NULL VARCHAR(256) 
+    video_name VARCHAR(256) NOT NULL
 );
+CREATE TYPE users_mark AS ENUM ('like', 'dislike');
 CREATE TABLE marks (
-    video_id int NOT NULL CONSTRAINT "MARKS VIDEOS" REFERENCES video,
-    user_id int NOT NULL REFERENCES users_notyoutube,
-    mark text NOT NULL CHECK(mark = 'like' OR mark = 'dislike')
+    video_id int CONSTRAINT "MARKS VIDEOS" REFERENCES video,
+    user_id int REFERENCES users_notyoutube,
+    mark users_mark NOT NULL,
+    PRIMARY KEY (video_id, user_id)
 );
 CREATE TABLE comments (
+    comments_id bigserial PRIMARY KEY,
     video_id int NOT NULL CONSTRAINT "COMMENT VIDEO" REFERENCES video,
     user_id int NOT NULL REFERENCES users_notyoutube,
     comments text NOT NULL CHECK(comments <> '')
 );
-ALTER TABLE users_notyoutube RENAME COLUMN id TO user_id;
-ALTER TABLE video RENAME COLUMN id TO video_id;
+DROP TABLE comments;
